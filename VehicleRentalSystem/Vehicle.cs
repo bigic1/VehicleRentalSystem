2d1b16536fa20410cdf05e5db39424e46dc2a7d7
Bosko <Bosko@DESKTOP-D9I3BA2>
2024-06-16 15:08:03 +0200

    Add project files.

---
+++ VehicleRentalSystem/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalSystem
+{
+    internal class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Value { get; set; }
+        public int Period { get; set; }
+
+        public Vehicle(string brand, string model, int value, int period) 
+        {
+            Brand = brand;
+            Model = model;
+            Value = value;
+            Period = period;
+
+        }
+    }
+}

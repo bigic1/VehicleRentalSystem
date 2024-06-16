10baa7db2b7bd803fe3bd2ef972a2cfc28003793
Bosko Igic <boskoigic@elfak.rs>
2024-06-17 01:09:11 +0200

    This should be the final version

--- VehicleRentalSystem/Invoice.cs
+++ VehicleRentalSystem/Invoice.cs
@@ -9,54 +9,56 @@ namespace VehicleRentalSystem
 {
     internal class Invoice
     {
-        public Invoice(Vehicle v) 
+        public Invoice(Vehicle v, DateTime invoicedate) 
         {
 
             Console.WriteLine("XXXXXXXXXX");
-            Console.WriteLine("Date:" + DateTime.Today.ToString("yyyy-MM-dd"));
+            Console.WriteLine("Date:" + invoicedate.ToString("yyyy-MM-dd"));
             Console.WriteLine("Customer Name: " + v.Customer);
             Console.WriteLine("Rented Vehicle: " + v.Brand + " " + v.Model);
             Console.WriteLine("");
             Console.WriteLine("Reservation start date: " + v.StartDate.ToString("yyyy-MM-dd"));
             Console.WriteLine("Reservation end date: " + v.EndDate.ToString("yyyy-MM-dd"));
-            int days = (v.EndDate - v.StartDate).Days;
-            Console.WriteLine("Reserved rental days: " + days);
+            int days = (v.EndDate - v.StartDate).Days; // rental days
+            Console.WriteLine("Reserved rental days: " + days + " days");
             Console.WriteLine("");
-            int actdays = (DateTime.Today.Date - v.StartDate).Days;
-            Console.WriteLine("Actual Return date: " + DateTime.Today.ToString("yyyy-MM-dd"));
-            Console.WriteLine("Actual rental days: " + actdays);
+            int actdays = (invoicedate - v.StartDate).Days; // actual rental days
+            Console.WriteLine("Actual return date: " + invoicedate.ToString("yyyy-MM-dd"));
+            Console.WriteLine("Actual rental days: " + actdays + " days");
             Console.WriteLine("");
-            Console.WriteLine("Rental cost per day:" + v.Cost);
-            double insurance = (v.Value * v.Insurance);
-            double increase = GetIncrease(v);
+            Console.WriteLine("Rental cost per day: $" + v.Cost.ToString("F")); //ToString("F") - 2 decimal places
+            double insurance = (v.Value * v.Insurance); // insurance cost per day
+            double increase = GetIncrease(v); //insurance price increase/discount as a faction of insurance price
             if (increase != 0)
             {
-                double inc = v.Insurance * v.Value * increase;
-                Console.WriteLine("Initial insurance per day: " + insurance);
-                Console.WriteLine("Insurance " + ((increase > 0) ? "increase" : "discount") + " per day: " + Math.Abs(inc));
-                insurance += inc;
+                double inc = insurance * increase; //insurance increase/discount per day
+                Console.WriteLine("Initial insurance per day: $" + insurance.ToString("F"));
+                Console.WriteLine("Insurance " + ((increase > 0) ? "increase" : "discount") + " per day: $" + Math.Abs(inc).ToString("F"));
+                insurance += inc; //the insurance price per day is being increased/reduced by the increase/discount
 
             }
-            Console.WriteLine("Insurance per day:" + insurance);
+            Console.WriteLine("Insurance per day: $" + insurance.ToString("F"));
             Console.WriteLine("");
-            double rent = v.Cost * days;
-            insurance *= days;
-            if (actdays < days)
+            double rent = v.Cost * days; // rent (without the early return discount)
+            insurance *= days; // insurance (without the early return discount)
+            if (actdays < days) // early return discount
             {
                 double rentdiscount;
                 double insdiscount;
                 int diff = days - actdays;
                 rentdiscount = (v.Cost * diff) / 2;
                 insdiscount = (insurance / days) * diff;
-                Console.WriteLine("Early return discount for rent: " + rentdiscount);
-                Console.WriteLine("Early return discount for insurance: " + insdiscount);
+                Console.WriteLine("Early return discount for rent: $" + rentdiscount.ToString("F"));
+                Console.WriteLine("Early return discount for insurance: $" + insdiscount.ToString("F"));
+                Console.WriteLine("");
                 rent -= rentdiscount;
                 insurance -= insdiscount;
             }
-            Console.WriteLine("Total rent: " + rent);
-            Console.WriteLine("Total insurance: " + insurance);
-            Console.WriteLine("Total: " + (rent+insurance));
-        
+            Console.WriteLine("Total rent: $" + rent.ToString("F"));
+            Console.WriteLine("Total insurance: $" + insurance.ToString("F"));
+            Console.WriteLine("Total: $" + (rent+insurance).ToString("F"));
+            Console.WriteLine("XXXXXXXXXX");
+
 
         }
         double GetIncrease(Vehicle v)
@@ -64,8 +66,8 @@ namespace VehicleRentalSystem
             double increase = 0;
             if (v.GetType() == typeof(Car))
             {
-                int rating = ((Car)v).Rating;
-                if (rating >= 4)
+                int rating = ((Car)v).Rating; //casting as the child class to access its property
+                if (rating >= 4) //high safety rating
                 {
                     increase = -0.1;
                 }
@@ -73,7 +75,7 @@ namespace VehicleRentalSystem
             else if (v.GetType() == typeof(Motorcycle))
             {
                 int age = ((Motorcycle)v).Age;
-                if (age < 25)
+                if (age < 25) //age under 25
                 {
                     increase = 0.2;
                 }
@@ -81,7 +83,7 @@ namespace VehicleRentalSystem
             else if (v.GetType() == typeof(CargoVan))
             {
                 int exp = ((CargoVan)v).Experience;
-                if (exp > 5)
+                if (exp > 5) //over 5 years experience
                 {
                     increase = -0.15;
                 }
--- VehicleRentalSystem/Program.cs
+++ VehicleRentalSystem/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Car veh1 = new("John Doe", "Mitsubishi", "Mirage", 15000, DateTime.Parse("June 07, 2024"), DateTime.Parse("June 17, 2024"), 3);
-            Motorcycle veh2 = new("Mary Johnson", "Triumph", "Tiger Sport 660", 10000, DateTime.Parse("June 07, 2024"), DateTime.Parse("June 17, 2024"), 20);
-            CargoVan veh3 = new("John Markson", "Citroen", "Jumper", 20000, DateTime.Parse("June 07, 2024"), DateTime.Parse("June 22, 2024"), 8);
-            Invoice inv1 = new(veh1);
-            Invoice inv2 = new(veh2);
-            Invoice inv3 = new(veh3);
+            Car veh1 = new("John Doe", "Mitsubishi", "Mirage", 15000, DateTime.Parse("June 03, 2024"), DateTime.Parse("June 13, 2024"), 3);
+            Motorcycle veh2 = new("Mary Johnson", "Triumph", "Tiger Sport 660", 10000, DateTime.Parse("June 03, 2024"), DateTime.Parse("June 13, 2024"), 20);
+            CargoVan veh3 = new("John Markson", "Citroen", "Jumper", 20000, DateTime.Parse("June 03, 2024"), DateTime.Parse("June 18, 2024"), 8);
+            Invoice inv1 = new(veh1, DateTime.Parse("June 13, 2024"));
+            Invoice inv2 = new(veh2, DateTime.Parse("June 13, 2024"));
+            Invoice inv3 = new(veh3, DateTime.Parse("June 13, 2024"));
         }
     }
 }
